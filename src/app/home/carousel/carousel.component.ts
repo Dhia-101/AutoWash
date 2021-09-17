c759452ed799a3bb3ac8e302e32b89d126874be2
DhiaTr <trabelsidhia19@gmail.com>
2021-09-17 14:21:18 +0100

    carousel refactor

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -9,9 +9,41 @@ export class CarouselComponent implements OnInit {
 
 
 
+  slides = [
+    { 
+      src: 'assets/img/carousel-1.jpg',
+      serviceName: 'Washing & Detailing',
+      promotionText: 'Keep your Car Newer',
+      text: 'Lorem ipsum dolor sit amet elit. Phasellus ut mollis mauris. Vivamus egestas eleifend dui ac'
+    },
+    { 
+      src: 'assets/img/carousel-2.jpg',
+      serviceName: 'Washing & Detailing',
+      promotionText: 'Keep your Car Newer',
+      text: 'Lorem ipsum dolor sit amet elit. Phasellus ut mollis mauris. Vivamus egestas eleifend dui ac'
+    },
+    { 
+      src: 'assets/img/carousel-3.jpg',
+      serviceName: 'Washing & Detailing',
+      promotionText: 'Keep your Car Newer',
+      text: 'Lorem ipsum dolor sit amet elit. Phasellus ut mollis mauris. Vivamus egestas eleifend dui ac'
+    }
+  ]
+
+  currentSlide = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  nextSlide() {
+    this.currentSlide = this.currentSlide === 2 ? 0 : this.currentSlide + 1;
+  }
+
+  previousSlide() {
+    this.currentSlide = this.currentSlide === 0 ? 2 : this.currentSlide - 1;
+  }
+
 
 }
