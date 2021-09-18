d8d119e15f526d664a14bccc225d86fa05964cf0
DhiaTr <trabelsidhia19@gmail.com>
2021-09-18 09:45:21 +0100

    carousel queries refactor

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss', './carousel.component.queries.scss']
 })
 export class CarouselComponent implements OnInit {
 // To FIX: animations not working
