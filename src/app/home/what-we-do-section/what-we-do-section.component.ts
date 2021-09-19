47dafc4b736eda3a1bc0c36e3dbe2129c6770e6b
DhiaTr <trabelsidhia19@gmail.com>
2021-09-19 13:17:04 +0100

    what we do section first breakpoint style

--- src/app/home/what-we-do-section/what-we-do-section.component.ts
+++ src/app/home/what-we-do-section/what-we-do-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-what-we-do-section',
   templateUrl: './what-we-do-section.component.html',
-  styleUrls: ['./what-we-do-section.component.scss']
+  styleUrls: ['./what-we-do-section.component.scss', './what-we-do-section.component.query.scss']
 })
 export class WhatWeDoSectionComponent implements OnInit {
 
