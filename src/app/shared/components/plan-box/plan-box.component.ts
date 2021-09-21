d61af56cb84b5fb60d1066c402d2fc50f2e73ee4
DhiaTr <trabelsidhia19@gmail.com>
2021-09-21 13:17:34 +0100

    washing plans large screen style

--- src/app/shared/components/plan-box/plan-box.component.ts
+++ src/app/shared/components/plan-box/plan-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-plan-box',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanBoxComponent implements OnInit {
 
+  @Input() shadow = true;
+  @Input() buttonType = 'main';
   constructor() { }
 
   ngOnInit(): void {
