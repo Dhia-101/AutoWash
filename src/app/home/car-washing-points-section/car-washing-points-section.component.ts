b1a9e95a89487b2c77fd9e1b4ccdb97a07702dc9
DhiaTr <trabelsidhia19@gmail.com>
2021-09-22 08:35:29 +0100

    car washing section small style adjus \n first 2 breakpoints style

--- src/app/home/car-washing-points-section/car-washing-points-section.component.ts
+++ src/app/home/car-washing-points-section/car-washing-points-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-car-washing-points-section',
   templateUrl: './car-washing-points-section.component.html',
-  styleUrls: ['./car-washing-points-section.component.scss']
+  styleUrls: ['./car-washing-points-section.component.scss', 'car-washing-points-section.component.query.scss']
 })
 export class CarWashingPointsSectionComponent implements OnInit {
 
