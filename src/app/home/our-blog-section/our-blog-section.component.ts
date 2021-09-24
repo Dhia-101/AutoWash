b719b692fb6b941cab46557045ec6e59a70dabbb
DhiaTr <trabelsidhia19@gmail.com>
2021-09-24 10:48:15 +0100

    article cards first two breakpoints\nsmallstyle adju

--- src/app/home/our-blog-section/our-blog-section.component.ts
+++ src/app/home/our-blog-section/our-blog-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-our-blog-section',
   templateUrl: './our-blog-section.component.html',
-  styleUrls: ['./our-blog-section.component.scss']
+  styleUrls: ['./our-blog-section.component.scss', './our-blog-section.component.query.scss']
 })
 export class OurBlogSectionComponent implements OnInit {
 
