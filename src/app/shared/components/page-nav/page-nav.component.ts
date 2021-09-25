b9376b9d14cb5320c045a95c18abb1db35d2436b
DhiaTr <trabelsidhia19@gmail.com>
2021-09-25 10:49:17 +0100

    page-nav component dynamic and style adjus

--- src/app/shared/components/page-nav/page-nav.component.ts
+++ src/app/shared/components/page-nav/page-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-page-nav',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-nav.component.scss']
 })
 export class PageNavComponent implements OnInit {
+  @Input() title = '';
+  @Input() items: any;
 
   constructor() { }
 
