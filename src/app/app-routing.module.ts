83ad505e6d8de631042088792dd7506867a447a1
DhiaTr <trabelsidhia19@gmail.com>
2021-09-25 10:13:45 +0100

    navbar links and style set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,22 @@ import {TeamMembersPageComponent} from './pages/team-members-page/team-members-p
 import {ScheduleBookingPageComponent} from './pages/schedule-booking-page/schedule-booking-page.component';
 import {AboutPageComponent} from './pages/about-page/about-page.component';
 import {DetailPageComponent} from './pages/detail-page/detail-page.component';
-import {WashingPointsComponent} from './washing-points/washing-points.component';
+import {WashingPointsComponent} from './pages/washing-points/washing-points.component';
 import {ServicePageComponent} from './pages/service-page/service-page.component';
 import {ContactPageComponent} from './pages/contact-page/contact-page.component';
 import {BlogGridPageComponent} from './pages/blog-grid-page/blog-grid-page.component';
 import {PricePageComponent} from './pages/price-page/price-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'service', component: ServicePageComponent },
   { path: 'price', component: PricePageComponent },
   { path: 'washing-points', component: WashingPointsComponent },
-  { path: 'blog-grid', component: BlogGridPageComponent },
-  { path: 'detail-page', component: DetailPageComponent },
-  { path: 'team-members', component: TeamMembersPageComponent },
-  { path: 'schedule-booking', component: ScheduleBookingPageComponent },
+  { path: 'more/blog-grid', component: BlogGridPageComponent },
+  { path: 'more/detail', component: DetailPageComponent },
+  { path: 'more/team-members', component: TeamMembersPageComponent },
+  { path: 'more/schedule-booking', component: ScheduleBookingPageComponent },
   { path: 'contact', component: ContactPageComponent },
 ];
 
