36085cab9411ed954acda478af9bb21fabeebb8d
DhiaTr <trabelsidhia19@gmail.com>
2021-09-26 11:42:49 +0100

    schedule booking page set

--- src/app/pages/schedule-booking-page/schedule-booking-page.component.ts
+++ src/app/pages/schedule-booking-page/schedule-booking-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-schedule-booking-page',
   templateUrl: './schedule-booking-page.component.html',
-  styleUrls: ['./schedule-booking-page.component.scss']
+  styleUrls: ['./schedule-booking-page.component.scss', 'schedule-booking-page.component.query.scss']
 })
 export class ScheduleBookingPageComponent implements OnInit {
 
