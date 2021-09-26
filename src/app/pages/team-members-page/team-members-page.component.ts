a55202e03e72f752c8c11ad23d8c3db71d202e8e
DhiaTr <trabelsidhia19@gmail.com>
2021-09-26 11:33:59 +0100

    meet our team page set

--- src/app/pages/team-members-page/team-members-page.component.ts
+++ src/app/pages/team-members-page/team-members-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-team-members-page',
   templateUrl: './team-members-page.component.html',
-  styleUrls: ['./team-members-page.component.scss']
+  styleUrls: ['./team-members-page.component.scss', 'team-members-page.component.query.scss']
 })
 export class TeamMembersPageComponent implements OnInit {
 
