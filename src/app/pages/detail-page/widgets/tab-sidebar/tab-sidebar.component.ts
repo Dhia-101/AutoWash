eff6ad4c7f6213de962828228b7946776ae8f95b
DhiaTr <trabelsidhia19@gmail.com>
2021-09-29 20:00:10 +0100

    tabs widget implementation , style and structure

--- src/app/pages/detail-page/widgets/tab-sidebar/tab-sidebar.component.ts
+++ src/app/pages/detail-page/widgets/tab-sidebar/tab-sidebar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-tab-sidebar',
   templateUrl: './tab-sidebar.component.html',
-  styleUrls: ['./tab-sidebar.component.scss']
+  styleUrls: ['./tab-sidebar.component.scss'],
+  animations: [
+    trigger('tabBodyAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('300ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class TabSidebarComponent implements OnInit {
 
+  selectedTab = 1;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ChangeTab(value: number): void {
+    this.selectedTab = value;
   }
 
 }
